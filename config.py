f7fd8d18ddbd672a45414d1179906a7259a42bff
crazyDeveloperz <133535122+crazyDeveloperz@users.noreply.github.com>
2023-09-28 22:23:32 -0700

    Update config.py

--- config.py
+++ config.py
@@ -2,23 +2,23 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "")
-API_HASH = os.environ.get("API_HASH", "")
+API_ID = os.environ.get("API_ID", "25069425")
+API_HASH = os.environ.get("API_HASH", "41034e257e6449615faea5f18bbe1dd7")
 #BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
-TOKEN_ONE = os.environ.get("TOKEN_ONE", "")
+TOKEN_ONE = os.environ.get("TOKEN_ONE", "6034105097:AAHssjQYwoQ7p6jhonOXJF6xHIVLh-mVJeM")
 
-CHANNEL = os.environ.get("CHANNEL", "ATL_Univers") # username without '@'
+CHANNEL = os.environ.get("CHANNEL", "Crazybotz") # username without '@'
 BOT_USERNAME = os.environ.get("BOT_USERNAME","") # username without '@'
-SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP","BYNF_TamilChat") # username without '@'
-UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL","ATL_Univers") # username without '@'
-OWNER_USERNAME = os.environ.get("OWNER_USERNAME","HMF_Owner_1")
+SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP","crazysupportz") # username without '@'
+UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL","Crazybotz") # username without '@'
+OWNER_USERNAME = os.environ.get("OWNER_USERNAME","heartlesssn")
 STRING = os.environ.get("STRING", "")
 
 DB_NAME = os.environ.get("DB_NAME","renameone")     
-DB_URL = os.environ.get("DB_URL","")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://Renamerplan:Renamerplan@cluster0.kkezdvj.mongodb.net/?retryWrites=true&w=majority")
 
 FLOOD = int(os.environ.get("FLOOD", "90"))
-LAZY_PIC = os.environ.get("LAZY_PIC", "")
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1989750989').split()]
+LAZY_PIC = os.environ.get("LAZY_PIC", "https://telegra.ph/file/6b003c4f05cb8609627d7.jpg")
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6249148586').split()]
 PORT = os.environ.get('PORT', '8080')
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001230390281"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001706887258"))
