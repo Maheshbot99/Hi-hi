95f100aa0e40dc72e9fe46dafa7af5ff79ad0c04
crazyDeveloperz <133535122+crazyDeveloperz@users.noreply.github.com>
2023-09-30 22:32:12 -0700

    Update progress.py

--- helper/progress.py
+++ helper/progress.py
@@ -23,12 +23,12 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "[{0}{1}] \n**Progress**: {2}%\n".format(
-            ''.join(["●" for i in range(math.floor(percentage / 5))]),
-            ''.join(["○" for i in range(20 - math.floor(percentage / 5))]),
+        progress = "[{0}{1}] \n**ᴄʀᴀᴢʏ ᴘʀᴏɢʀᴇꜱꜱ**: {2}%\n".format(
+            ''.join(["█" for i in range(math.floor(percentage / 5))]),
+            ''.join(["░" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "{0} of {1}\n**Speed**: {2}/s\n**ETA**: {3}\n".format(
+        tmp = progress + "**✅ ᴄᴏᴍᴘʟᴇᴛᴇᴅ** {0}\n**📁 ᴛᴏᴛᴀʟ ꜱɪᴢᴇ** {1}\n**🚀 ꜱᴘᴇᴇᴅ**: {2}/s\n**⏰ ᴇᴛᴀ**: {3}\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
@@ -37,9 +37,10 @@ async def progress_for_pyrogram(
         )
         try:
             await message.edit(
-                text="{}\n {}".format(
-                    ud_type,
-                    tmp
+                text="{}\n\n{}".format(ud_type, tmp),               
+                reply_markup=InlineKeyboardMarkup( [[
+                    InlineKeyboardButton("✖️ 𝙲𝙰𝙽𝙲𝙴𝙻 ✖️", callback_data="cancel")
+                    ]]
                 )
             )
         except:
